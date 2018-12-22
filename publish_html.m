a5574839a08a322987b4063cfd763a49864beb46
Michael Holmboe <michael.holmboe@umu.se>
2018-12-22 16:14:29 +0100

    New functions for healing/fusing/protonating sites, reading pdb trajectories with changing number of atoms etc.
    Version 2.0

--- publish_html.m
+++ publish_html.m
@@ -11,14 +11,10 @@ dirs={...
     };
 
 for j=1:size(dirs,1)
-    
     cd(char(dirs(j)))
     filelist = dir('*.m')
-    
     for i=1:size(filelist,1)
         publish(filelist(i).name,'evalCode',false,'outputDir','/Users/miho0052/Dropbox/MATLAB/Matlab_scripts/functions/atom/html')
     end
-    
     cd ..
-    
 end
