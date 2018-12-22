04f7d98947e6d0536d321f274c434946969d61df
Michael Holmboe <michael.holmboe@umu.se>
2018-12-22 16:20:16 +0100

    Add files via upload

---
+++ PATH2VMD.m
@@ -0,0 +1,7 @@
+%% The VMD path on your computer
+function PATH2VMD = PATH2VMD()
+
+% Add your own path to VMD here. Note that you might need a '\' to skip spaces
+
+PATH2VMD = '/Applications/VMD\ 1.9.2.app/Contents/MacOS/startup.command'; 
+
