517d1cd2bfd3a0800063884d38b209f14021b688
Michael Holmboe <michael.holmboe@umu.se>
2021-01-07 02:00:16 +0100

    Version 2.09

--- ATOM_scripts_lecture/INTERFACE15_3W_NaMMT/MMT_interface/buildscripts/buildclay.m
+++
@@ -1,8 +0,0 @@
-%% Import a .car file from Heinz's MODEL database in his INTERFACE 1.5 distribution
-atom = import_atom_car('mont0_333_K_15_single_layer.car','no_counterions') % Remove the counterions, we will add them later when building the enitre system
-
-% Is the charges correct?
-% Before starting the simulation, make sure the topol.top file is correct,
-% and note that you must change the moleculename in each .itp file, i.e.
-% mon should be changed to MMT
-
--- examples/Build_a_2xlayered_system/Advanced_examples.m
+++
@@ -1,9 +0,0 @@
-%% Advanced examples
-% *Here we build enitre systems...*
-%
-% _Todo - create a clay system_
-%
-% * <Buildclay.html Buildclay>
-% * <BuildSystem_bilayered.html BuildSystem_bilayered>
-
-
